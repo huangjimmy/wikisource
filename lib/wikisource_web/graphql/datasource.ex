76b230265c3935017d6b69951abdba66c185c889
huangsj <jimmy_huang@live.com>
2019-09-10 18:54:02 +0800

    fix dataloader batch load for book chapters
    now correctly load first 10 chapters of each book.  see lib/wikisource_web/graphql/datasource.ex

--- lib/wikisource_web/graphql/datasource.ex
+++ lib/wikisource_web/graphql/datasource.ex
@@ -8,12 +8,14 @@ defmodule WikisourceWeb.DataSource do
   end
 
   def query(queryable, params) do
-    IO.inspect(queryable)
-    IO.inspect(params)
     case params do
-      %{order_by: order_by, from: from, size: size} -> from record in queryable, order_by: ^order_by, offset: ^from, limit: ^size
-      %{from: from, size: size} -> from record in queryable, offset: ^from, limit: ^size
-      %{order_by: order_by, from: from} -> from record in queryable, order_by: ^order_by, offset: ^from, limit: 10
+      %{chapters: true, offset: offset, first: first} ->
+        last = offset + first
+        query = from r in queryable, select: r, select_merge: %{chapter_number: fragment("row_number() over (PARTITION by parent_id order by \"name\")")}
+        from r in subquery(query), select: %Wikisource.Book{id: r.id, name: r.name, info: r.info, preface: r.preface, info_html: r.info_html, preface_html: r.preface_html}, where: r.chapter_number >= ^offset and r.chapter_number < ^last
+      %{order_by: order_by, offset: from, first: size} -> from record in queryable, order_by: ^order_by, offset: ^from, limit: ^size
+      %{offset: from, first: size} -> from record in queryable, offset: ^from, limit: ^size
+      %{order_by: order_by, offset: from} -> from record in queryable, order_by: ^order_by, offset: ^from, limit: 10
       %{order_by: order_by} -> from record in queryable, order_by: ^order_by, offset: 0, limit: 10
       %{} -> queryable
     end
