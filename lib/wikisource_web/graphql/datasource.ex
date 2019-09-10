107469541eed787c4cb06a47159e60b33ae31adc
huangsj <jimmy_huang@live.com>
2019-09-10 21:47:09 +0800

    use window function instead of ecto sql fragment
    row_number() |> over(partition_by: r.parent_id, order_by: r.name)

--- lib/wikisource_web/graphql/datasource.ex
+++ lib/wikisource_web/graphql/datasource.ex
@@ -11,7 +11,7 @@ defmodule WikisourceWeb.DataSource do
     case params do
       %{chapters: true, offset: offset, first: first} ->
         last = offset + first
-        query = from r in queryable, select: r, select_merge: %{chapter_number: fragment("row_number() over (PARTITION by parent_id order by \"name\")")}
+        query = from r in queryable, select: r, select_merge: %{chapter_number: row_number() |> over(partition_by: r.parent_id, order_by: r.name)}
         from r in subquery(query), select: %Wikisource.Book{id: r.id, name: r.name, info: r.info, preface: r.preface, info_html: r.info_html, preface_html: r.preface_html}, where: r.chapter_number >= ^offset and r.chapter_number < ^last
       %{order_by: order_by, offset: from, first: size} -> from record in queryable, order_by: ^order_by, offset: ^from, limit: ^size
       %{offset: from, first: size} -> from record in queryable, offset: ^from, limit: ^size
