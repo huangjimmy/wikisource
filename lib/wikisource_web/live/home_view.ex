f72c0c52198f035d602f602a9b5fb4bf6a74c7d4
huangsj <jimmy_huang@live.com>
2019-09-14 11:11:22 +0800

    add loading indicator to /app
    see app.css, home_view.ex and home.html.leex

--- lib/wikisource_web/live/home_view.ex
+++ lib/wikisource_web/live/home_view.ex
@@ -3,9 +3,9 @@ defmodule Wikisource.SearchQuery do
   import Ecto.Changeset
 
   schema "search_query" do
-    field :query, :string
-    field :_utf8, :string
-    field :page, :integer
+    field(:query, :string)
+    field(:_utf8, :string)
+    field(:page, :integer)
   end
 
   @doc false
@@ -26,43 +26,75 @@ defmodule WikisourceWeb.HomeView do
 
   def mount(_session, socket) do
     query = %Wikisource.SearchQuery{}
-    {:ok, assign(socket, changeset: Wikisource.SearchQuery.changeset(query, %{query: ""}), page_content: nil, page: 1, query: "" )}
+
+    {:ok,
+     assign(socket,
+       changeset: Wikisource.SearchQuery.changeset(query, %{query: ""}),
+       page_content: nil,
+       page: 1,
+       query: "",
+       loading: false
+     )}
   end
 
   def handle_event("search", value, socket) do
     query = %Wikisource.SearchQuery{}
-    %{ assigns: %{ page: page, query: old_query_string } } = socket
-    %{"search_query" => %{ "query" => query_string } } = value
+    %{assigns: %{page: page, query: old_query_string}} = socket
+    %{"search_query" => %{"query" => query_string}} = value
 
-    page = if old_query_string == query_string do
-      page
-    else
-      1
-    end
+    page =
+      if old_query_string == query_string do
+        page
+      else
+        1
+      end
 
-    params = %{ "query" => query_string, "page" => page }
+    socket =
+      assign(socket,
+        query: query_string,
+        page: page,
+        changeset: Wikisource.SearchQuery.changeset(query, %{query: query_string})
+      )
 
-    {query_string, page_content} = case query_string do
-      "" -> {query_string, nil}
-      _ -> WikisourceWeb.PageController.search(params)
-    end
-    {:noreply, assign(socket, page_content: page_content, query: query_string, changeset: Wikisource.SearchQuery.changeset(query, %{query: query_string})) }
+    socket =
+      case query_string do
+        "" ->
+          assign(socket, loading: false, page_content: nil)
+
+        _ ->
+          send(self(), :search)
+          assign(socket, loading: true)
+      end
+
+    {:noreply, socket}
   end
 
   def handle_event("page", value, socket) do
     query = %Wikisource.SearchQuery{}
-    %{ assigns: %{ query: query_string } } = socket
+    %{assigns: %{query: query_string}} = socket
 
     query_string = value |> Map.get("search_query", %{}) |> Map.get("query", query_string)
 
-    %{"search_query" => %{ "page" => page } } = value
-    params = %{ "query" => query_string, "page" => page }
+    %{"search_query" => %{"page" => page}} = value
 
-    {query_string, page_content} = case query_string do
-      "" -> {query_string, nil}
-      _ -> WikisourceWeb.PageController.search(params)
-    end
-    {:noreply, assign(socket, page_content: page_content, page: page, query: query_string, changeset: Wikisource.SearchQuery.changeset(query, %{query: query_string}))}
+    socket =
+      assign(socket,
+        page: page,
+        query: query_string,
+        changeset: Wikisource.SearchQuery.changeset(query, %{query: query_string})
+      )
+
+    socket =
+      case query_string do
+        "" ->
+          assign(socket, loading: false, page_content: nil)
+
+        _ ->
+          send(self(), :search)
+          assign(socket, loading: true)
+      end
+
+    {:noreply, socket}
   end
 
   def handle_event("validate", _value, socket) do
@@ -70,17 +102,27 @@ defmodule WikisourceWeb.HomeView do
   end
 
   def handle_params(params, _uri, socket) do
-
-    %{ assigns: %{ page: _page, query: old_query_string } } = socket
+    %{assigns: %{page: _page, query: old_query_string}} = socket
 
     query = Map.get(params, "query", old_query_string)
     page = Map.get(params, "page", 1)
+
     case {query, page} do
-      { "", _ } ->
+      {"", _} ->
         {:noreply, socket}
+
       _ ->
-        handle_event("page", %{"search_query" => %{ "page" => page, "query" => query } }, socket)
+        socket = assign(socket, loading: true)
+        handle_event("page", %{"search_query" => %{"page" => page, "query" => query}}, socket)
     end
   end
 
+  def handle_info(:search, socket) do
+    %{assigns: %{query: query_string, page: page}} = socket
+
+    {_, page_content} =
+      WikisourceWeb.PageController.search(%{"query" => query_string, "page" => page})
+
+    {:noreply, assign(socket, page_content: page_content, loading: false)}
+  end
 end
