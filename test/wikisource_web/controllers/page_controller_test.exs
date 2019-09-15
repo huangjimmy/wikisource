ffa73af985c93800147473b71fc4a0f4a0dd4754
huangsj <jimmy_huang@live.com>
2019-09-16 01:47:13 +0800

    update unit test
    @see test/wikisource_web/controllers/page_controller_test.exs

--- test/wikisource_web/controllers/page_controller_test.exs
+++ test/wikisource_web/controllers/page_controller_test.exs
@@ -3,6 +3,11 @@ defmodule WikisourceWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "b_searchboxForm"
+  end
+
+  test "GET /app", %{conn: conn} do
+    conn = get conn, "/app"
+    assert html_response(conn, 200) =~ "b_searchboxForm"
   end
 end
